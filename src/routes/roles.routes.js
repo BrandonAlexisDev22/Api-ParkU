4df7d2195e01a154b79db380eebe42d1c947a26d
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2026-03-07 18:27:53 -0500

    feat: adding new routes

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const roleController = require('../controllers/roles.controller');
 
-router.post('/', roleController.createRole);
-router.get('/', roleController.getRoles);
+router.post('/create', roleController.createRole);
+router.put('/edit/:id', roleController.editRole);
+router.delete('/delete/:id', roleController.deleteRole)
+router.get('/get', roleController.getRoles);
 
 module.exports = router;
