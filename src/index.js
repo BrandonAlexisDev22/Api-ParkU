153126695c0d716dd2a1cedfbf087d07e6d48891
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2026-03-07 17:08:00 -0500

    delete: remove commentary

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express(); // 👈 primero se declara
+const app = express(); 
 
 // ─── Middlewares globales ───────────────────────────────────────────
 app.use(cors());
