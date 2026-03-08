d2303b83c3a791d604aef56b21d659d15aff01db
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2026-03-07 20:37:54 -0500

    fix: correct module name and update Permiso class parameters in permisos model

--- src/models/permisos.models.js
+++ src/models/permisos.models.js
@@ -1,7 +1,7 @@
 /**
- * @module PermisoModels
+ * @module PermisoModel
  * @description
- * Clase que representa la entidad Role dentro del sistema ParkU.
+ * Clase que representa la entidad Permiso dentro del sistema ParkU.
  * Modela la estructura de un permiso en la aplicación.
  */
 
@@ -9,20 +9,23 @@
  * Representa un permiso dentro del sistema.
  *
  * @class Permiso
- * @param {number} id - Identificador único del permisos
- * @param {string} rol_id - id del rol asociado
- * @param {Array<string>} permisos - Lista de modulos asociados al permiso
- * @param {boolean} [estado=true] - Estado del rol (activo o inactivo)
+ *
+ * @param {number} id_permiso - Identificador único del permiso
+ * @param {string} nombre_permiso - Nombre del permiso
+ * @param {string} modulo - Módulo al que pertenece el permiso
+ * @param {boolean} [estado=true] - Estado del permiso (activo o inactivo)
  */
 
 class Permiso {
-    constructor(id,rol_id,modulo,estado){
-        this.id = id;
-        this.rol_id = rol_id,
-        this.modulo = modulo,
-        this.estado = estado
+
+    constructor(id_permiso, nombre_permiso, modulo, estado = true) {
+        this.id_permiso = id_permiso;
+        this.nombre_permiso = nombre_permiso;
+        this.modulo = modulo;
+        this.estado = estado;
     }
+
 }
 
-
 module.exports = Permiso;
+
