a0489e2f92e06b5cfa478264a776d1bf2113f385
cano0102 <canoanderson418@gmail.com>
2026-03-16 02:28:28 -0500

    feat: db configuration

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise";
+
+/**
+ * Conexión a la base de datos MySQL en Railway
+ */
+const connection = mysql.createPool({
+  host: "yamanote.proxy.rlwy.net",
+  user: "root",
+  password: "TU_PASSWORD",
+  database: "railway",
+  port: 28484,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+export default connection;
