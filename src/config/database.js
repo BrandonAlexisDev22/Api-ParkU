7ae21053b9fa179680a47f3b5e626d958ae59154
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2026-07-23 10:39:06 -0500

    feat: Migrate database configuration from MySQL to PostgreSQL using pg

--- src/config/database.js
+++ src/config/database.js
@@ -1,51 +1,15 @@
-const mysql = require("mysql2/promise"); // Usamos la versión con promesas
-
-// Configuración del pool de conexiones
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "12345678",
-  database: process.env.DB_NAME || "parku",
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+// CONFIGURACION DE LA BASE DE DATOS DESDE NEON (POSTGRESQL)
+import { Pool } from "pg";
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// Función para probar la conexión
-const testConnection = async () => {
-  try {
-    const connection = await pool.getConnection();
-    console.log("Conexión a MySQL establecida correctamente.");
-    connection.release();
-    return true;
-  } catch (error) {
-    console.error("❌ Error al conectar a MySQL:", error.message);
-    return false;
-  }
-};
 
-// Función para ejecutar consultas
-const query = async (sql, params = []) => {
-  try {
-    const [rows, fields] = await pool.execute(sql, params);
-    return rows;
-  } catch (error) {
-    console.error("❌ Error en consulta SQL:", error.message);
-    throw error;
-  }
-};
+//CONSULTA DE PRUEBA
+const result = await pool.query("SELECT * FROM users");
+console.log(result.rows);
 
-// Función para obtener una conexión del pool (para transacciones)
-const getConnection = async () => {
-  return await pool.getConnection();
-};
-
-module.exports = {
-  pool,
-  query,
-  getConnection,
-  testConnection,
-};
+export default pool;
